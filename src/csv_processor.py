f8b50f4f59fe21006e2764b22fb24fc9e1414085
saadira <rafsan.saadi@verizon.com>
2020-04-12 00:07:19 -0700

    saving progress

--- src/csv_processor.py
+++ src/csv_processor.py
@@ -70,11 +70,13 @@ class CsvProcessor:
             for invoice in invoice_id_lst:
                 if line[0] == invoice:
                     print("Matched item details: {}".format(line))
-                    item_dict[invoice] = line
+                    item_dict[line[1]] = line
 
         if len(item_dict) == 0:
             print("No matched ITEM record found")
         else:
+            # for k, v in item_dict.items():
+            #     print(k, v)
             return item_dict
         # #
         # # for k, v in item_dict.items():
@@ -84,10 +86,28 @@ class CsvProcessor:
 
     def total_calculator(self, item_dictionary):
         print("Calculating total amount...")
+        total = []
+        amounts = []
+        clean_amount = []
         if item_dictionary is not None:
             for k, v in item_dictionary.items():
-                print(type(v))
-                print(v)
+                # print(type(v))
+                # print(v[2])
+                amounts.append(v[2])
+
+        # Cleaning up the amount
+        for item in amounts:
+            clean_amount.append(item.replace('”', ''))
+
+        # Converting amount to num/float
+        for i in range(0, len(clean_amount)):
+            # Expecting float decimal points issue
+            clean_amount[i] = float(clean_amount[i])
+
+        total.append(sum(clean_amount))
+        print("total: {}".format(total))
+        return total
+
 
 
 
@@ -97,20 +117,18 @@ reader = CsvProcessor()
 
 # customer_id = reader.customer_processor(reader.customer_generator(),
 #                                         ['”Olivia”','”Johnson”'])
-
 customer_id = reader.customer_processor(reader.customer_generator(),
                                         ['”Indiana”','”Jones”'])
-print(customer_id)
-
 # customer_id = reader.customer_processor(reader.customer_generator(),
 #                                         ['”Harper”','”Moore”'])
-# print(customer_id)
+print(customer_id)
 
 invoice_id = reader.invoice_processor(reader.invoice_generator(), customer_id)
 print(invoice_id)
 
+# items = reader.item_processor(reader.item_generator(), ['“IN0999987”','“IN0999999”'])
 
-items = reader.item_processor(reader.item_generator(), ['“IN0999987”','“IN0999999”'])
+items = reader.item_processor(reader.item_generator(), invoice_id)
 
 total = reader.total_calculator(items)
 
