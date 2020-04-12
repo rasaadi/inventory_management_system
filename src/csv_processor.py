81fcb4a8f6cc3d17af7020b7075b97f5fa42f485
saadira <rafsan.saadi@verizon.com>
2020-04-11 23:38:38 -0700

    saving progress

---
+++ src/csv_processor.py
@@ -0,0 +1,107 @@
+import os
+import sys
+import csv
+
+sys.path.append(os.path.join(os.path.dirname(__file__)))
+sys.path.append(os.path.join(os.path.dirname(__file__), '../'))
+from src import resource_config
+
+
+class CsvProcessor:
+    def __init__(self):
+        self.customer_file = resource_config.CUSTOMER_FILE_PATH
+        self.invoice_file = resource_config.INVOICE_FILE_PATH
+        self.item_file = resource_config.ITEM_FILE_PATH
+
+    def customer_generator(self):
+        print("Generating Customer Information...")
+        with open(self.customer_file, 'rt') as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',', )
+            next(csv_reader)
+            for line in csv_reader:
+                yield line
+                # print(row)
+
+    def customer_processor(self, customer_file_gen, customer_name):
+        print("Processing Customer Information...")
+        for line in customer_file_gen:
+            if line[1] == customer_name[0] and line[2] == customer_name[1]:
+                print("Matched customer details: {}".format(line))
+                return line[0]  # Customer ID
+
+    def invoice_generator(self):
+        print("Generating Invoice Information...")
+        with open(self.invoice_file, 'rt') as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',', )
+            next(csv_reader)
+            for line in csv_reader:
+                yield line
+                # print(row)
+
+    def invoice_processor(self, invoice_generator, customer_id):
+        print("Processing Invoice Information...")
+        invoice_list = []
+        for line in invoice_generator:
+            # print(line)
+            if line[0] == customer_id:
+                print("Matched invoice details: {}".format(line))
+                invoice_list.append(line[1])
+
+        if len(invoice_list) == 0:
+            print("No matched INVOICE record found")
+        else:
+            return invoice_list
+
+    def item_generator(self):
+        print("Generating Item Information...")
+        with open(self.item_file, 'rt') as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',', )
+            next(csv_reader)
+            for line in csv_reader:
+                yield line
+                # print(row)
+
+    def item_processor(self, invoice_generator, invoice_id_lst):
+        print("Processing Item Information...")
+        # print("Invoice id: {}".format(invoice_id_lst))
+        item_dict = {}
+        for line in invoice_generator:
+            # print(line)
+            for invoice in invoice_id_lst:
+                if line[0] == invoice:
+                    print("Matched item details: {}".format(line))
+                    item_dict[invoice] = line
+
+        if len(item_dict) == 0:
+            print("No matched ITEM record found")
+        else:
+            return item_dict
+        # #
+        # # for k, v in item_dict.items():
+        # #     print("Printing item dictionary")
+        # #     print(k, v)
+        # return item_dict
+
+
+
+
+
+reader = CsvProcessor()
+
+# customer_id = reader.customer_processor(reader.customer_generator(),
+#                                         ['”Olivia”','”Johnson”'])
+
+customer_id = reader.customer_processor(reader.customer_generator(),
+                                        ['”Indiana”','”Jones”'])
+print(customer_id)
+
+# customer_id = reader.customer_processor(reader.customer_generator(),
+#                                         ['”Harper”','”Moore”'])
+# print(customer_id)
+
+invoice_id = reader.invoice_processor(reader.invoice_generator(), customer_id)
+print(invoice_id)
+
+
+items = reader.item_processor(reader.item_generator(), ['”IN0500000”'])
+
