7ded77f006118c1c168fae212224cce1f2b4b48b
saadira <rafsan.saadi@verizon.com>
2020-04-12 00:27:13 -0700

    added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Inventory management system',
+    version='1.0.0',
+    author='Rafsan Saadi',
+    author_email='rafsan.saadi@gmail.com',
+    packages=find_packages(),
+    description='Inventory management system',
+)
